016cdb8c6986964f1e2ee0bcc2d831bf8b6477a9
NuclMe <saltansergei@gmail.com>
2025-02-23 02:31:04 +0200

    fixed gulp errors

--- gulpfile.js
+++ gulpfile.js
@@ -1,168 +1,143 @@
-var gulp            = require('gulp');
-var sass            = require('gulp-sass');
-var less            = require('gulp-less');
-var watch           = require('gulp-watch');
-var sourcemaps      = require('gulp-sourcemaps');
-var autoprefixer    = require('gulp-autoprefixer');
-var clean           = require('gulp-clean');
-var handlebars      = require('gulp-compile-handlebars');
-var rename          = require('gulp-rename');
-var include         = require("gulp-include");
-var plumber         = require('gulp-plumber');
-var notify          = require('gulp-notify');
-var gulpif          = require('gulp-if');
-var zip             = require('gulp-zip');
+import gulp from 'gulp'
+import sassPkg from 'gulp-sass'
+import dartSass from 'sass'
+import less from 'gulp-less'
+import watch from 'gulp-watch'
+import sourcemaps from 'gulp-sourcemaps'
+import autoprefixer from 'gulp-autoprefixer'
+import { deleteAsync } from 'del'
+import handlebars from 'gulp-compile-handlebars'
+import rename from 'gulp-rename'
+import include from 'gulp-include'
+import plumber from 'gulp-plumber'
+import notify from 'gulp-notify'
+import gulpif from 'gulp-if'
+import zip from 'gulp-zip'
+import path from 'path'
+import browserSyncPkg from 'browser-sync'
 
-var path            = require('path');
-var browserSync     = require('browser-sync').create();
-var runSequence     = require('run-sequence');
+const browserSync = browserSyncPkg.create()
+const sass = sassPkg(dartSass)
+const { series, parallel } = gulp
 
-var paths = {
-  src:        './src',
-  dest:       './dist',
-  scss:       '/scss',
-  css:       '/css',
-  less:       '/less',
-  js:         '/js',
-  images:     '/images',
+const paths = {
+  src: './src',
+  dest: './dist',
+  scss: '/scss',
+  css: '/css',
+  less: '/less',
+  js: '/js',
+  images: '/images',
   components: '/components',
-  fonts:      '/fonts'
-};
+  fonts: '/fonts',
+}
 
-var isDebug = process.env.NODE_ENV !== 'production';
+const isDebug = process.env.NODE_ENV !== 'production'
 
-gulp.task('html', function () {
-  options = {
+// HTML task
+const html = function () {
+  const options = {
     ignorePartials: true,
     batch: [paths.src + paths.components],
     helpers: {
-      capitals: function(str){
-        return str.toUpperCase();
-      }
-    }
+      capitals: function (str) {
+        return str.toUpperCase()
+      },
+    },
   }
-  return gulp.src(paths.src + '/*.{html,hbs,handlebars}')
-    .pipe(plumber({
-        errorHandler: notify.onError("Error: <%= error.message %>")
-    }))
+  return gulp
+    .src(paths.src + '/*.{html,hbs,handlebars}')
+    .pipe(plumber({ errorHandler: notify.onError('Error: <%= error.message %>') }))
     .pipe(handlebars({}, options))
-    .pipe(rename(function (path) {
-      path.extname = ".html"
-    }))
+    .pipe(
+      rename((path) => {
+        path.extname = '.html'
+      }),
+    )
     .pipe(gulp.dest(paths.dest))
-    .pipe(browserSync.stream());
-});
+    .pipe(browserSync.stream())
+}
 
-gulp.task('scss', function() {
-    return gulp.src(paths.src + paths.scss + '/**/*.{scss,sass,css}')
-        .pipe(plumber({
-            errorHandler: notify.onError("Error: <%= error.message %>")
-        }))
-        .pipe(gulpif(isDebug, sourcemaps.init()))
-        .pipe(sass({
-            includePaths: [
-                __dirname + "/bower_components",
-                __dirname + "/node_modules"
-            ]
-        }).on('error', sass.logError))
-        .pipe(autoprefixer())
-        .pipe(gulpif(isDebug, sourcemaps.write()))
-        .pipe(gulp.dest(paths.dest + '/css'))
-        .pipe(browserSync.stream());
-});
+// SCSS task
+const scss = function () {
+  return gulp
+    .src(paths.src + paths.scss + '/**/*.{scss,sass,css}')
+    .pipe(plumber({ errorHandler: notify.onError('Error: <%= error.message %>') }))
+    .pipe(gulpif(isDebug, sourcemaps.init()))
+    .pipe(sass({ includePaths: ['node_modules'] }).on('error', sass.logError))
+    .pipe(autoprefixer())
+    .pipe(gulpif(isDebug, sourcemaps.write()))
+    .pipe(gulp.dest(paths.dest + '/css'))
+    .pipe(browserSync.stream())
+}
 
-gulp.task('less', function() {
-    return gulp.src(paths.src + paths.less + '/**/*.less')
-        .pipe(plumber({
-            errorHandler: notify.onError("Error: <%= error.message %>")
-        }))
-        .pipe(gulpif(isDebug, sourcemaps.init()))
-        .pipe(less({
-            paths: [
-                path.join(__dirname, 'less', 'includes'),
-                __dirname + "/bower_components",
-                __dirname + "/node_modules"
-            ]
-        }))
-        .pipe(autoprefixer())
-        .pipe(gulpif(isDebug, sourcemaps.write()))
-        .pipe(gulp.dest(paths.dest + '/css'))
-        .pipe(browserSync.stream());
-});
+// LESS task
+const lessTask = function () {
+  return gulp
+    .src(paths.src + paths.less + '/**/*.less')
+    .pipe(plumber({ errorHandler: notify.onError('Error: <%= error.message %>') }))
+    .pipe(gulpif(isDebug, sourcemaps.init()))
+    .pipe(less({ paths: ['node_modules'] }))
+    .pipe(autoprefixer())
+    .pipe(gulpif(isDebug, sourcemaps.write()))
+    .pipe(gulp.dest(paths.dest + '/css'))
+    .pipe(browserSync.stream())
+}
 
-gulp.task('js', function() {
-    return gulp.src(paths.src + paths.js + '/main.js')
-        .pipe(plumber({
-            errorHandler: notify.onError("Error: <%= error.message %>")
-        }))
-        .pipe(include({
-          includePaths: [
-            __dirname + "/bower_components",
-            __dirname + "/node_modules"
-          ]
-        }))
-        .pipe(gulp.dest(paths.dest + paths.js))
-        .pipe(browserSync.stream());
-});
+// Fonts task
+const fonts = function () {
+  return gulp
+    .src(paths.src + paths.fonts + '/**/*')
+    .pipe(gulp.dest(paths.dest + '/fonts'))
+    .pipe(browserSync.stream())
+}
 
-gulp.task('js:vendor', function() {
-    return gulp.src(paths.src + paths.js + '/vendor/**/*.{js,json}')
-        .pipe(gulp.dest(paths.dest + paths.js))
-        .pipe(browserSync.stream());
-});
+// Images task
+const images = function () {
+  return gulp
+    .src(paths.src + paths.images + '/**/*')
+    .pipe(gulp.dest(paths.dest + '/images'))
+    .pipe(browserSync.stream())
+}
 
-gulp.task('images', function() {
-    return gulp.src(paths.src + paths.images + '/**/*.{png,jpg,svg}')
-        .pipe(gulp.dest(paths.dest + paths.images))
-        .pipe(browserSync.stream());
-});
+// JS task
+const scripts = function () {
+  return gulp
+    .src(paths.src + paths.js + '/**/*.js')
+    .pipe(gulp.dest(paths.dest + '/js'))
+    .pipe(browserSync.stream())
+}
 
-gulp.task('fonts', function() {
-    return gulp.src(paths.src + paths.scss + paths.fonts + '/**/*.{eot,ttf,svg,otf,woff}')
-        .pipe(gulp.dest(paths.dest + paths.css + paths.fonts))
-        .pipe(browserSync.stream());
-});
+// Clean task
+const clean = function () {
+  return deleteAsync(paths.dest)
+}
 
-gulp.task('zip', function() {
-    gulp.src(paths.dest + '/**/*.*')
-        .pipe(zip(path.basename(__dirname) + '.zip'))
-        .pipe(gulp.dest(paths.dest))
-});
+// Serve task (browserSync)
+const serve = function () {
+  browserSync.init({
+    server: {
+      baseDir: paths.dest,
+    },
+    port: 3000,
+    open: true,
+    notify: false,
+  })
+}
 
-gulp.task('clean', function() {
-    return gulp.src(paths.dest + '/')
-        .pipe(clean({ force: true }));
-});
+// Watch task
+const watchFiles = function () {
+  watch([paths.src + '/**/*.html', './src/data/data.json'], html)
+  watch(paths.src + paths.scss + '/**/*.scss', scss)
+  watch(paths.src + paths.less + '/**/*.less', lessTask)
+  watch(paths.src + paths.fonts + '/**/*', fonts)
+  watch(paths.src + paths.images + '/**/*', images)
+  watch(paths.src + paths.js + '/**/*.js', scripts)
+  serve()
+}
 
-gulp.task('watch', function() {
-    watch([paths.src + '/**/*.html', './src/data/data.json'], function() {
-        gulp.start('html');
-    });
-    watch(paths.src + paths.scss + '/**/*.scss', function() {
-        gulp.start('scss');
-    });
-    watch(paths.src + paths.less + '/**/*.less', function() {
-        gulp.start('less');
-    });
-    watch(paths.src + paths.js + '/**/*.{js,json}', function() {
-        gulp.start('js');
-    });
-    watch(paths.src + paths.js + '/vendor/**/*.{js,json}', function() {
-        gulp.start('js:vendor');
-    });
-    watch(paths.src + paths.images + '/**/*.{png,jpg,svg}', function() {
-        gulp.start('images');
-    });
-});
+// Export tasks
+export { html, scss, lessTask, fonts, images, scripts, clean, watchFiles, serve }
 
-gulp.task('default', ['scss', 'less', 'html', 'js', 'js:vendor', 'images', 'fonts'], function() {
-    browserSync.init({
-        server: paths.dest
-    });
-    gulp.start('watch');
-});
-
-gulp.task('build', function() {
-    runSequence('clean', ['scss', 'less', 'html', 'js', 'js:vendor', 'images', 'fonts'],
-      'zip');
-});
+// Default task
+export default series(clean, parallel(scss, lessTask, html, fonts, images, scripts), watchFiles)
